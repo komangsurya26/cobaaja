a7ebfb9b9478f7323b63df32ee46a5a8055170a7
komangsurya26 <119924783+komangsurya26@users.noreply.github.com>
2023-02-16 15:57:17 +0800

    tambah kode

--- get-user.js
+++ get-user.js
@@ -1,44 +1,41 @@
-const fs = require ('fs')
-const path = require ('path')
-const readlinesync = require ('readline-sync')
+const fs = require('fs');
+const readlinesync = require('readline-sync');
 
+const semuaData = './database/users.json';
 
-const dataPath = path.join(__dirname, './database/users.json')
-
-let database = { users: [] }
+const database = [] 
 
 try {
-  const data = fs.readFileSync(dataPath, 'utf8')
-  database.users = JSON.parse(data)
+  const data = fs.readFileSync(semuaData)
+  database = JSON.parse(data)
 } catch (err) {
   console.error(err)
 }
 
 const getAllUser = () => {
-    if (database.users.length === 0) {
-      console.log('Tidak ada pengguna yang tersedia')
-      return
-    }
-    console.clear()
-    console.log('Daftar pengguna:\n')
-    console.log('====================\n')
-    console.log('====================\n')
-
-    database.users.forEach((users,index) => {
-
-      console.log(`USER NO ${index+1}\n
-      NAMA: ${users.nama}\n
-      UMUR: ${users.umur} tahun\n
-      ALAMAT: ${users.alamat}\n`
-      )
-      
-    })
-
-    const optionKembali = readlinesync.question('\n\nsilahkan tekan 2 untuk kembali ')
-    console.clear()
-
-    return optionKembali
+  if (database.length === 0) {
+    console.log('Tidak ada pengguna yang tersedia')
+    return
   }
-  
-  module.exports = { getAllUser}
-  
+  console.clear()
+  console.log('Daftar pengguna:\n')
+  console.log('====================\n')
+  console.log('====================\n')
+
+  database.forEach((users,index) => {
+
+    console.log(`USER NO ${index+1}\n
+    NAMA: ${users.nama}\n
+    UMUR: ${users.umur} tahun\n
+    ALAMAT: ${users.alamat}\n`
+    )
+    
+  })
+
+  const optionKembali = readlinesync.question('\n\nsilahkan tekan 2 untuk kembali ')
+  console.clear()
+
+  return optionKembali
+}
+
+module.exports = { getAllUser }
